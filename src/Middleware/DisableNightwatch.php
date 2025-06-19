ca5565753281229d7e7a5a9d726f32abc5803e9c
Tyler Satre <tyler.satre@gmail.com>
2025-06-18 22:16:35 -0400

    Add middleware to disable Nightwatch logging (#1)
    * Add middleware to disable Nightwatch logging
    
    * refactor: improve middleware tests

---
+++ src/Middleware/DisableNightwatch.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Laravel\Nightwatch\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Laravel\Nightwatch\Core;
+use Laravel\Nightwatch\State\CommandState;
+use Laravel\Nightwatch\State\RequestState;
+
+final class DisableNightwatch
+{
+    /**
+     * @param  Core<RequestState|CommandState>  $nightwatch
+     */
+    public function __construct(private readonly Core $nightwatch) {}
+
+    public function handle(Request $request, Closure $next): mixed
+    {
+        $this->nightwatch->shouldSample = false;
+        $this->nightwatch->shouldSampleOnException = false;
+
+        return $next($request);
+    }
+}
